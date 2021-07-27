dbd4aadba2c10b13109c2fc4034523e0e68c7cd8
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-07-27 21:14:39 +0700

    'update'

--- src/main/java/vinhnq27/springframework/springbootapplication/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package vinhnq27.springframework.springbootapplication.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import vinhnq27.springframework.springbootapplication.model.Author;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- src/main/java/vinhnq27/springframework/springbootapplication/repositories/PublisherRepository.java
+++
@@ -1,8 +0,0 @@
-package vinhnq27.springframework.springbootapplication.repositories;
-
-
-import org.springframework.data.repository.CrudRepository;
-import vinhnq27.springframework.springbootapplication.model.Publisher;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
