476665816d67203af425830f092e0e5e8d77cae6
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-07-29 20:49:26 +0700

    add POJO

---
+++ src/main/java/vinhnq27/springframework/springbootapplication/model/Owner.java
@@ -0,0 +1,7 @@
+package vinhnq27.springframework.springbootapplication.model;
+
+public class Owner extends Person{
+    public Owner(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
---
+++ src/main/java/vinhnq27/springframework/springbootapplication/model/Vet.java
@@ -0,0 +1,7 @@
+package vinhnq27.springframework.springbootapplication.model;
+
+public class Vet extends Person{
+    public Vet(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
