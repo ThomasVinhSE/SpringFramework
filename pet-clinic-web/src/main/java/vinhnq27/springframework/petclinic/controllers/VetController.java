8a2b2a425ea415668a492309b7bdfd50bb7cfa90
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-08-06 10:26:44 +0700

    bootstrap vet list

--- pet-clinic-web/src/main/java/vinhnq27/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/vinhnq27/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,21 @@
 package vinhnq27.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vinhnq27.springframework.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String listVets() {
-      return "vets/index";
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
     };
 }
