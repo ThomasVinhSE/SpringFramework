2a0b83d4a2a983cb9575eff9a3306251e4bc600e
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-08-15 11:22:49 +0700

    update properties in owner

--- pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Owner.java
@@ -3,7 +3,39 @@ package vinhnq27.springframework.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+    public Owner() {
+    }
+
+    public Owner(Long id, String firstName, String lastName) {
+        super(id, firstName, lastName);
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -13,10 +45,4 @@ public class Owner extends Person{
         this.pets = pets;
     }
 
-    public Owner() {
-    }
-
-    public Owner(Long id, String firstName, String lastName) {
-        super(id, firstName, lastName);
-    }
 }
