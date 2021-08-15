68cabe1f710bba1d77e759bb5fbf76acf62e979c
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-08-15 11:10:08 +0700

    add visit, updated owner with pet relationship

--- pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Owner.java
@@ -1,6 +1,18 @@
 package vinhnq27.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
     public Owner() {
     }
 
