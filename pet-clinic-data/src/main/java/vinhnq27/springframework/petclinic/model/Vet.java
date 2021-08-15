0ceaded160d59fed2f4075cf0a54e954e7eb61d6
ThomasVinhSE <74160788+ThomasVinhSE@users.noreply.github.com>
2021-08-15 11:19:03 +0700

    add speciality, added to vet

--- pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/vinhnq27/springframework/petclinic/model/Vet.java
@@ -1,10 +1,21 @@
 package vinhnq27.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
     public Vet() {
     }
 
     public Vet(Long id, String firstName, String lastName) {
         super(id, firstName, lastName);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
